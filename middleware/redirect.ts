1dbc4a75cb45d307645b3997dce588f136417db5
GeniusCorn <hi@nicecorn.com>
2024-02-01 23:01:51 +0800

    fix: `count visits` should be moved to server side

--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -1,21 +1,11 @@
-import { db } from '~/db'
-import { codeToURL } from '~/db/schema'
-
 export default defineNuxtRouteMiddleware(async (to) => {
   const code = to.path.split('/').at(-1)
 
   try {
     const url: string = await $fetch(`/api/${code}`)
 
-    const findUniqueCode = await db.query.codeToURL.findFirst({
-      where: (codeToURL, { eq }) => eq(codeToURL.code, code as string),
-    })
-
-    await db.update(codeToURL).set({
-      visits: findUniqueCode!.visits! + 1,
-    })
-
-    navigateTo(url, {
+    await navigateTo(url, {
+      redirectCode: 301,
       external: true,
     })
   }
--- server/api/[code].get.ts
+++ server/api/[code].get.ts
@@ -1,18 +1,24 @@
 import { db } from '~/db'
+import { codeToURL } from '~/db/schema'
 
 export default defineEventHandler(async (event) => {
   const code = getRouterParam(event, 'code')
 
-  const findURL = await db.query.codeToURL.findFirst({
-    where: (codeToURL, { eq }) => eq(codeToURL.code, code as string),
-  })
+  try {
+    const findURL = await db.query.codeToURL.findFirst({
+      where: (codeToURL, { eq }) => eq(codeToURL.code, code as string),
+    })
 
-  if (!findURL) {
+    await db.update(codeToURL).set({
+      visits: findURL!.visits! + 1,
+    })
+
+    return findURL?.url
+  }
+  catch (error) {
     throw createError({
       statusCode: 404,
       statusMessage: 'Code Not Found',
     })
   }
-
-  return findURL.url
 })
