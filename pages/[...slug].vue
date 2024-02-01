1e66b45b14c3608bdd526a4ee4dc52b841017cfa
GeniusCorn <hi@nicecorn.com>
2024-02-01 22:29:43 +0800

    style: improve the styling of slug page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,11 +2,25 @@
 definePageMeta({
   middleware: 'redirect',
 })
+
+const route = useRoute()
+
+const code = computed(() => route.params.slug.at(0))
 </script>
 
 <template>
-  <div>
-    {{ $route.params.slug }} not Found
+  <div class="w-96 bg-base-100 shadow-xl card">
+    <div class="flex flex-col gap-4 card-body">
+      <div text-center>
+        {{ code }} Not Found
+      </div>
+
+      <div class="divider" />
+
+      <NuxtLink class="btn btn-primary" to="/">
+        Back To Home
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
