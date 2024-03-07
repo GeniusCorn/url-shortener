f158942f66c180eb87bfa84b0c740d06a49383d8
GeniusCorn <hi@nicecorn.com>
2024-03-07 23:31:14 +0800

    chore: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  devServer: {
+    port: 5000,
+  },
   modules: ['@unocss/nuxt', '@vueuse/nuxt'],
   css: [
     '@unocss/reset/tailwind.css',
